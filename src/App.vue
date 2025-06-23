e34fec0f1d582807e152999c3bcdd7759ad803ec
BAYUEQI <zouqi050813@gmail.com>
2025-06-23 17:36:16 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,17 @@
   <div class="min-h-screen flex flex-col">
     <HeaderComponent class="py-4" />
     <CoverGenerator class="flex-1" />
-    <FooterComponent />
   </div>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import CoverGenerator from './components/CoverGenerator.vue';
-import FooterComponent from './components/FooterComponent.vue';
 
 export default {
   components: {
     HeaderComponent,
-    CoverGenerator,
-    FooterComponent
+    CoverGenerator
   }
 };
 </script>
