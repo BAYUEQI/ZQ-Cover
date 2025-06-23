d0ec1007b4f3babb99f143e45adaaa31fb6b8ae3
BAYUEQI <zouqi050813@gmail.com>
2025-06-23 17:46:27 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="min-h-screen flex flex-col">
-    <HeaderComponent class="py-4" />
-    <CoverGenerator class="flex-1" />
+  <div class="min-h-screen flex flex-col bg-gradient-to-br from-gray-50 to-gray-100">
+    <HeaderComponent class="py-4 shadow-md" />
+    <CoverGenerator class="flex-1 transition-all duration-300 ease-in-out" />
   </div>
 </template>
 
@@ -16,3 +16,22 @@ export default {
   }
 };
 </script>
+
+<style>
+:root {
+  --vue-app-font-family: system-ui, -apple-system, "Segoe UI", Roboto, sans-serif;
+}
+
+body {
+  font-family: var(--vue-app-font-family);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+/* 添加全局过渡效果 */
+.transition-all {
+  transition-property: all;
+  transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+  transition-duration: 300ms;
+}
+</style>
